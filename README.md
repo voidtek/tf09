016addbc95b2cee68694e1c7dcb7ea318ace8497
Victor DA COSTA <vdacosta@voidtek.com>
2016-06-07 15:36:31 +0000

    Provides setup batch

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# tf09
+# tf
 
-SETUP
----
+## Setup
 Run the script setup.sh
+This batch includes:
+- pip
+- tensorflow
 
+For more details about TensorFlow installation, you can check [TensorFlow Installation Guide](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/g3doc/get_started/os_setup.md)
+
+## Tested
+Environement:
+- Cloud9
+
+## Dependencies
+```
+python
+
+numpy
+matplotlib
+cuda
+tflearn (if using tflearn examples)
+```
--- setup.sh
+++ setup.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-echo "Check the Python version"
-python --version
+echo "--------------------------------"
+echo ". "
+echo "[+] Install python-dev"
+sudo apt-get install python-dev
+
+echo "[+] Install python-pip"
+sudo apt-get remove --auto-remove python-pip -y
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo ln -s /usr/local/bin/pip /usr/bin/pip
+sudo rm -rf get-pip.py
 
 echo "--------------------------------"
 echo ". "
 
-echo "Check th pip is installed"
-pip list
+echo "[+] Install TensorFlow"
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp27-none-linux_x86_64.whl
+sudo pip install --upgrade $TF_BINARY_URL
+
+echo "--------------------------------------------------------------"
+echo ". "
+
+echo "[+] Install a few extra packages"
+sudo pip install --upgrade matplotlib
+sudo pip install --upgrade scikit-learn
+sudo pip install --upgrade git+git://github.com/tensorflow/skflow.git
+
+echo "--------------------------------------------------------------"
+echo ". "
+
+echo "[+] Install a folder with examples"
+git clone https://github.com/aymericdamien/TensorFlow-Examples.git TensorFlow-Examples

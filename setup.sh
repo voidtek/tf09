5679b5ddd1d7ef1e758982a06cf9cb927ae4da60
Victor DA COSTA <vdacosta@voidtek.com>
2016-06-07 16:45:26 +0000

    Adding tests and libraries

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 echo "--------------------------------"
 echo ". "
-echo "[+] Install python-dev"
-sudo apt-get install python-dev
 
 echo "[+] Install python-pip"
 sudo apt-get remove --auto-remove python-pip -y
@@ -11,6 +9,10 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo ln -s /usr/local/bin/pip /usr/bin/pip
 sudo rm -rf get-pip.py
+
+echo "[+] Install python-dev"
+sudo apt-get install libhdf5-dev
+sudo apt-get install python-pip python-dev
 
 echo "--------------------------------"
 echo ". "
@@ -26,9 +28,12 @@ echo "[+] Install a few extra packages"
 sudo pip install --upgrade matplotlib
 sudo pip install --upgrade scikit-learn
 sudo pip install --upgrade git+git://github.com/tensorflow/skflow.git
+sudo pip install --upgrade git+https://github.com/tflearn/tflearn.git
+sudo pip install --upgrade h5py
 
 echo "--------------------------------------------------------------"
 echo ". "
 
 echo "[+] Install a folder with examples"
 git clone https://github.com/aymericdamien/TensorFlow-Examples.git TensorFlow-Examples
+git clone https://github.com/tflearn/tflearn.git tflearn
